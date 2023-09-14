84bd7744885e439d284f682dd8e889d121b49e6a
natashaaoliveira <140986828+natashaaoliveira@users.noreply.github.com>
2023-09-14 16:14:35 -0300

    add commit desafio diagramação em UML

---
+++ src/Iphone/AparelhoTelefonico.java
@@ -0,0 +1,8 @@
+package iphone.telefone;
+
+public interface AparelhoTelefonico {
+
+    void ligar();
+    void atender();
+    void iniciarCorrerioVoz();
+}
---
+++ src/Iphone/main.java
@@ -0,0 +1,19 @@
+import iphone.iphone;
+
+public class main {
+    public static void main(String[] args) {
+        iphone iphone = new iphone();
+        System.out.println("-------------------------------------");
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorrerioVoz();
+        System.out.println("-------------------------------------");
+        iphone.pausar();
+        iphone.tocar();
+        iphone.selecionarMusica();
+        System.out.println("-------------------------------------");
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+        iphone.exibirPagina();
+    }
+}
